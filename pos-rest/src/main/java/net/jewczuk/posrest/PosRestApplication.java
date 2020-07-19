b97327c1b0eeb743bb9826a9d46fbc956418ccfd
Michal <michal.jewczuk@outlook.com>
2020-07-19 12:39:56 +0200

    Remove unused import

--- pos-rest/src/main/java/net/jewczuk/posrest/PosRestApplication.java
+++ pos-rest/src/main/java/net/jewczuk/posrest/PosRestApplication.java
@@ -3,7 +3,6 @@ package net.jewczuk.posrest;
 import net.jewczuk.posdomain.config.PosDomainConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
