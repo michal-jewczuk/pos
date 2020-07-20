3ac00d0c1a97b9b3625c8ea7c536b2004cc15f4a
Michal Jewczuk <michal.jewczuk@capgemini.com>
2020-07-20 08:56:25 +0200

    Change paths for products

--- pos-rest/src/main/java/net/jewczuk/posrest/controller/ProductController.java
+++ pos-rest/src/main/java/net/jewczuk/posrest/controller/ProductController.java
@@ -1,12 +1,13 @@
 package net.jewczuk.posrest.controller;
 
 import lombok.RequiredArgsConstructor;
-import net.jewczuk.posdomain.entity.Product;
 import net.jewczuk.posrest.dto.ProductDto;
 import net.jewczuk.posrest.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -17,8 +18,20 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping(path = "/")
+    @GetMapping("")
     public List<ProductDto> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @GetMapping("/")
+    public RedirectView redirectToList() {
+        return new RedirectView("/api/products");
+    }
+
+    @GetMapping("/{id}")
+    public ProductDto getSingleProduct(@PathVariable String id) {
+        Long productId = Long.parseLong(id);
+
+        return productService.getProductById(productId);
     }
 }
