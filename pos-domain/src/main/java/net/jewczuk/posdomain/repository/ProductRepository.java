408ec9ee9e8df1a26240c2aec0095d0bb368d7b5
Michal Jewczuk <michal.jewczuk@capgemini.com>
2020-07-21 09:45:45 +0200

    Add custom method with tests to Product repository

--- pos-domain/src/main/java/net/jewczuk/posdomain/repository/ProductRepository.java
+++ pos-domain/src/main/java/net/jewczuk/posdomain/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package net.jewczuk.posdomain.repository;
 import net.jewczuk.posdomain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllProductsByCategoryName(String name);
 }
---
+++ pos-domain/src/test/java/net/jewczuk/posdomain/repository/ProductRepositoryTest.java
@@ -0,0 +1,41 @@
+package net.jewczuk.posdomain.repository;
+
+import net.jewczuk.posdomain.config.PosDomainConfig;
+import net.jewczuk.posdomain.entity.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(classes = PosDomainConfig.class)
+@DataJpaTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    @DisplayName("should return products that have given category")
+    public void shouldReturnNonEmptyList() {
+        String category = "red";
+
+        List<Product> products = repository.findAllProductsByCategoryName(category);
+
+        assertThat(products.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("should return empty list for nonexistent category")
+    public void shouldReturnEmptyList() {
+        String category = "blue";
+
+        List<Product> products = repository.findAllProductsByCategoryName(category);
+
+        assertThat(products).isEmpty();
+    }
+}
