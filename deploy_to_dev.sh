5d44dc57f8610892b300b347e619e54a2c1ca982
Michal Jewczuk <michal.jewczuk@capgemini.com>
2020-07-28 15:21:16 +0200

    Add jar listing to deploy_to_dev

--- deploy_to_dev.sh
+++ deploy_to_dev.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+restPath='pos-rest/build/libs'
+domainPath='pos-domain/build/libs'
+jarPattern='SNAPSHOT\|RELEASE'
+
 echo "Deplyoing to dev "
 
-pwd
-ls -alt
-
+echo ">>> list jars from rest and domain"
+ls $restPath | grep $jarPattern
+ls $domainPath | grep $jarPattern
